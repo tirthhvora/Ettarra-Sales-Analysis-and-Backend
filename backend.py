91daa48789e3283d224e0b3ae4cc9e8e7610799c
Tirth Vora <tirthdatascience@gmail.com>
2024-02-17 20:36:11 +0530

    update: Modified feature_importance logic

--- backend.py
+++ backend.py
@@ -1,13 +1,26 @@
 from flask import Flask, jsonify
 import json
+import pandas as pd
+import pickle
+from sklearn.metrics import mean_squared_error
 
 app = Flask(__name__)
 
+
+with open('rf_model.pkl', 'rb') as file:
+    rf_model = pickle.load(file)
+
+X_test = pd.read_csv('testing_data.csv') 
+
 @app.route('/feature_importances', methods=['GET'])
 def get_feature_importances():
-    with open('feature_importance1.json', 'r') as json_file:
-        data = json.load(json_file)
-    return jsonify(data)
+        
+        feature_importances = pd.DataFrame({'Feature': X_test.columns, 'Importance': rf_model.feature_importances_})
+        feature_importances_sorted = feature_importances.sort_values(by='Importance', ascending=False)
+
+        data = feature_importances_sorted.to_dict(orient='records')
+        return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
